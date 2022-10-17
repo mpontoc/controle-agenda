d608b1dc3778427eb6886931855b99af372bc08f
mpontoc <mpontoc@hotmail.com>
2022-10-16 21:39:03 -0300

    new configs

--- src/main/kotlin/io/github/controleagenda/ControleagendaApplication.kt
+++ src/main/kotlin/io/github/controleagenda/ControleagendaApplication.kt
@@ -3,8 +3,10 @@ package io.github.controleagenda
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = ["io.github.*"])
 @EnableJpaAuditing
 class ControleAgendaApplication
 
